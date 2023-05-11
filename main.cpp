b55a8069fde8ddc1b614b739fdd7663fff8022dd
b1129004 <120005659+b1129004@users.noreply.github.com>
2023-05-11 14:21:18 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+class Fueltank{
+     private:
+     int m_FueltankCapacity;
+     int m_G_grade;
+     public:
+     Fueltank(FueltankCapacity=3000, int Gas=98){}
+     int fuel_up(int v, int gas){};
+     int set_Gas_grade(int Gas grade){}
+     int get_Gas_grade(){};
+     private:
+     Engine m_Engine;
+     Fueltank m_Fueltank;
+     int m_MaxSeating;
+     int m_price;
+     int m_base;
+     string m_brand;
+     string m_model;
+     int m_year;
+     void UpdatePrice(int base=500000)
+     public:
+     Car(string x,string y,int z,int s){}
+     int get_Gas_grade(){}
+     int set_Gas_grade(int gas=98){}
+     int fuel_up(int v, int gas=98){}
+    
+     
